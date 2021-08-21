12948a2c3bb1a3ff0f4c7eee4c6fb3edab798fde
jillbowen <jillian.b.short@gmail.com>
2021-08-21 14:29:09 -0700

    configure production environment

--- config/environment.rb
+++ config/environment.rb
@@ -10,10 +10,19 @@ configure :development do
   )
 end
 
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
+
+end
 
 require './app/controllers/application_controller'
 require_all 'app'
